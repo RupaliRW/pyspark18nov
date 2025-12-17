9d91109c8d8a665e7e28547c759ff0f5ba7591be
RupaliRW <rupalirwaykole@gmail.com>
2025-12-17 20:11:44 +0530

    first commit

---
+++ src/wcarg.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+import sys
+
+spark = SparkSession.builder.appName("RDDWordCountFromFile").getOrCreate()
+sc = spark.sparkContext
+
+rdd = sc.textFile(sys.argv[0])
+
+word_count = (
+    rdd.flatMap(lambda line: line.split(" "))
+       .map(lambda word: (word, 1))
+       .reduceByKey(lambda a, b: a + b)
+       .sortBy(lambda x: x[1], ascending=False)
+)
+
+for w in word_count.collect():
+    print(w)
+
